4a720eabd666fc2ce25b7638bea3fda9a392c026
GodsaintBHH26 <kabirajarijit40@gmail.com>
2025-10-07 11:33:48 +0530

    deplaoyment time

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:3000/api", 
-  headers:{
-    "Content-Type":"application/json"
-  }
- });
+const API = axios.create({
+  baseURL: `${
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:3000"
+  }/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
 // Attach token automatically
 API.interceptors.request.use((req) => {
