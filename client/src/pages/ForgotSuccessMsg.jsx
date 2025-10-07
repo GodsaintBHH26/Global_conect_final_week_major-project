d9fa927081d8089dd68a3bc0589e0e62c855577b
GodsaintBHH26 <kabirajarijit40@gmail.com>
2025-10-07 11:47:33 +0530

    another naming conflict

--- client/src/pages/ForgotSuccessMsg.jsx
+++ client/src/pages/ForgotSuccessMsg.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../styles/ForgotSuccessMsg.module.css';
+import styles from '../styles/forgotSuccessMsg.module.css';
 
 const ForgotSuccessMsg = () => {
   return (
